5a49fe7b0e47cbbaf5ce6cde2a94d9920f8e6c7b
adejolaO <aogunsan@cmitsouth.org>
2024-09-19 11:04:51 -0500

    debugging sticky scroll

--- scripts/ts/dynamic-dom/do-something.js
+++ scripts/ts/dynamic-dom/do-something.js
@@ -1,3 +1,14 @@
 export function doSomething() {
-  console.log("Put some code in here!");
+  window.onscroll = function () { myFunction(); };
+  var header = document.getElementById("navbar");
+  var sticky = header.offsetTop;
+  function myFunction() {
+      if (window.scrollY-1 > sticky) {
+          header.classList.add("sticky");
+      }
+      else {
+          header.classList.remove("sticky");
+      }
+  }
+  console.log("sticky-scroll!");
 }
